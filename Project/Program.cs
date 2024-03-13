0a26115eb7b187af9ce04cb507a864c7858538f3
TheEliteD <dhristov06@gmail.com>
2024-03-13 22:11:54 +0200

    damn
    apparently I needed to create a web app lel

--- Project/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.WriteLine("Yup, we are cooked :skull:");
-Console.WriteLine("Idk what we are supposed to di here :D");
-//Oh yes we are
-//Sooo brat zashto mi pishesh na Console WriteLine kato mojesh da pishesh na comment?
-
-/*
- * brat nqmash si ideq
- * kolko nz ko da pravq
-*/
