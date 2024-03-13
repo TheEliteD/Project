c00ac286050289e877ee6b695b8ac09d97618ef8
D3a7hKn1gh7 <angelpisarev@gmail.com>
2024-03-13 20:12:55 +0200

    test commit 2
    test 2

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,3 +1,4 @@
 ﻿Console.WriteLine("Yup, we are cooked :skull:");
 Console.WriteLine("Idk what we are supposed to di here :D");
 //Oh yes we are
+//Sooo brat zashto mi pishesh na Console WriteLine kato mojesh da pishesh na comment?
