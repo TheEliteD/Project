9403ded7d1176ffc7feb26f23694165b88dd38a5
TheEliteD <dhristov06@gmail.com>
2024-03-13 20:50:09 +0200

    wow

--- Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ca0767b9761471e7b2f78a194ee526a9db984b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e84845b4b0e937758166377d37117a8ee678a553")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
