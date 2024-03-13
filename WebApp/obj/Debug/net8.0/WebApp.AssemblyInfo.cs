83f3337907016d95ea4a769b45bfbb566734fdb8
D3a7hKn1gh7 <angelpisarev@gmail.com>
2024-03-13 22:53:41 +0200

    commit test 1
    test

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70dc09900af77b8f5c7f29851df54cdf1d7929aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e33126133d7a87d27961ed7968c0ebd875785412")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
