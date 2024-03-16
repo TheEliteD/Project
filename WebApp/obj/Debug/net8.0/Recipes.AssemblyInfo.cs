b3cf26674f3a0382a15bb48c7674c879eb1f4736
TheEliteD <dhristov06@gmail.com>
2024-03-16 19:34:36 +0200

    Recipes

--- WebApp/obj/Debug/net8.0/Recipes.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/Recipes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Recipes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+041c057e7e79b0cbd7f3e699b069b8ce8a6531f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df66cdbf30861e5b11519cdccbc87822050c7471")]
 [assembly: System.Reflection.AssemblyProductAttribute("Recipes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Recipes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
