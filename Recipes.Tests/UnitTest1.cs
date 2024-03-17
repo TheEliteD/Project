02d239d937c18fd4090de1da568bcd0101a255d6
TheEliteD <dhristov06@gmail.com>
2024-03-17 10:25:26 +0200

    Added the tests project

---
+++ Recipes.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Recipes.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
