91005304cbc980ceb41f0acf24bad3fd6b94fd91
D3a7hKn1gh7 <angelpisarev@gmail.com>
2024-03-17 20:02:52 +0200

    Added requirements for the properties
    In Chefs' section

--- Recipes/Models/Chefs.cs
+++ Recipes/Models/Chefs.cs
@@ -1,9 +1,17 @@
-﻿namespace Recipes.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Recipes.Models
 {
+    //Basic model for the chefs with their reviews
     public class Chefs
     {
+        [Key]
         public int Id {get; set;}
+
+        [Required]
         public string ChefReviews{get; set;}
+
+        [Required]
         public string TopChefs{get; set;}
 
         public Chefs()
