94575b7cc214660a1f6e0c3d32515df1f05d7217
D3a7hKn1gh7 <angelpisarev@gmail.com>
2024-03-20 23:24:22 +0200

    Removed one singular "?"
    From login.cshtml

--- Recipes/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Recipes/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -46,7 +46,7 @@ namespace Recipes.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Remember me")]
             public bool RememberMe { get; set; }
         }
 
